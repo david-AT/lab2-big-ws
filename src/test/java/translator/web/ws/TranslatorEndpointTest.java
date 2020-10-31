05ca076048479b95f7cd4784ce1ed19b7f41610e
David Alloza Tejero <davidallozatejero@MacBook-Pro-de-David.local>
2020-10-31 13:03:06 +0100

    Tests expected exceptions

--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -11,6 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ClassUtils;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
+import org.springframework.ws.client.WebServiceTransportException;
+
 import translator.Application;
 import translator.web.ws.schema.GetTranslationRequest;
 import translator.web.ws.schema.GetTranslationResponse;
@@ -36,8 +38,9 @@ public class TranslatorEndpointTest {
     marshaller.afterPropertiesSet();
   }
 
-  @Test
-  public void testSendAndReceive() {
+  // Exception thrown during the normal operation
+  @Test(expected=RuntimeException.class)
+  public void testRemoteWebServiceException() {
     GetTranslationRequest request = new GetTranslationRequest();
     request.setLangFrom("en");
     request.setLangTo("es");
@@ -49,4 +52,20 @@ public class TranslatorEndpointTest {
     GetTranslationResponse translation = (GetTranslationResponse) response;
     assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
   }
+
+  // Exception thrown whenever a transport error occurs on the client-side
+  @Test(expected=WebServiceTransportException.class)
+  public void testRemoteWebServiceDown() {
+    GetTranslationRequest request = new GetTranslationRequest();
+    request.setLangFrom("en");
+    request.setLangTo("es");
+    request.setText("This is a test of translation service");
+    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+            + port + "/wsIsDown", request);
+    assertNotNull(response);
+    assertThat(response, instanceOf(GetTranslationResponse.class));
+    GetTranslationResponse translation = (GetTranslationResponse) response;
+    assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+  }
+
 }
